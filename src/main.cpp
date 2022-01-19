383992f3a51934dc8f28262505b50fa03afe59c5
let02-desktop-linux <let024568@gmail.com>
2022-01-20 02:31:16 +0800

    change something not crucial

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,14 @@
 
 int main()
 {
-    std::cout<<"1\n";
     Blockchain bChain = Blockchain();
-    std::cout<<"2\n";
+
     cout << "Mining block 1..." << endl;
     bChain.AddBlock(Block(1, "Block 1 Data"));
-    std::cout<<"3\n";
+
     cout << "Mining block 2..." << endl;
     bChain.AddBlock(Block(2, "Block 2 Data"));
-    std::cout<<"4\n";
+
     cout << "Mining block 3..." << endl;
     bChain.AddBlock(Block(3, "Block 3 Data"));
 
