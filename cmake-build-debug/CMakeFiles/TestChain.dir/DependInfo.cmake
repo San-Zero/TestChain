9cbd27e0bdc0cb25915f36305010d385fb4d7e5c
let02 <let024568@gmail.com>
2022-03-07 22:31:48 +0800

    add cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/TestChain.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/let02/CLionProjects/Practice/TestChain/tools/Fruneng/sha256.c" "C:/Users/let02/CLionProjects/Practice/TestChain/cmake-build-debug/CMakeFiles/TestChain.dir/tools/Fruneng/sha256.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/include"
+  "../tools"
+  "../tools/OpenCL"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/let02/CLionProjects/Practice/TestChain/src/main.cpp" "C:/Users/let02/CLionProjects/Practice/TestChain/cmake-build-debug/CMakeFiles/TestChain.dir/src/main.cpp.obj"
+  "C:/Users/let02/CLionProjects/Practice/TestChain/src/modules/Block.cpp" "C:/Users/let02/CLionProjects/Practice/TestChain/cmake-build-debug/CMakeFiles/TestChain.dir/src/modules/Block.cpp.obj"
+  "C:/Users/let02/CLionProjects/Practice/TestChain/src/modules/BlockChain.cpp" "C:/Users/let02/CLionProjects/Practice/TestChain/cmake-build-debug/CMakeFiles/TestChain.dir/src/modules/BlockChain.cpp.obj"
+  "C:/Users/let02/CLionProjects/Practice/TestChain/src/modules/Transaction.cpp" "C:/Users/let02/CLionProjects/Practice/TestChain/cmake-build-debug/CMakeFiles/TestChain.dir/src/modules/Transaction.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/include"
+  "../tools"
+  "../tools/OpenCL"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TestChain.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestChain.dir/src/main.cpp.obj"
+  "CMakeFiles/TestChain.dir/src/modules/Block.cpp.obj"
+  "CMakeFiles/TestChain.dir/src/modules/BlockChain.cpp.obj"
+  "CMakeFiles/TestChain.dir/src/modules/Transaction.cpp.obj"
+  "CMakeFiles/TestChain.dir/tools/Fruneng/sha256.c.obj"
+  "TestChain.exe"
+  "TestChain.exe.manifest"
+  "TestChain.pdb"
+  "libTestChain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/TestChain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
